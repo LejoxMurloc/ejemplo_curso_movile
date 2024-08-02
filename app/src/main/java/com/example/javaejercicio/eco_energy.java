7bbb78c452b31a9620104e090ff5f40b54142291
Sergio Lopez <salopeza@udistrital.edu.co>
2024-08-01 19:37:22 -0500

    cracion y mod de eco y otros

--- app/src/main/java/com/example/javaejercicio/eco_energy.java
+++ app/src/main/java/com/example/javaejercicio/eco_energy.java
@@ -11,22 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
-
+public class eco_energy extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.eco_energy);
 
-        Button btnInicio = findViewById(R.id.btn_siguiente);
-        btnInicio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intento = new Intent(login.this, eco_energy.class);
-                startActivity(intento);
-            }
-        });
+
 
 
     }
